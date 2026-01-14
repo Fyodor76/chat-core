62172006ccb5cbf63e1d24bbd154c264f6005c1a
FydorZbinyakov <fydorzbinyakov@gmail.com>
2026-01-14 23:52:52 +0300

    Fix зависимостей

--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -3,9 +3,11 @@ import { ChatGateway } from './socket.gateway';
 import { ChatSessionService } from './application/services/chat-session.service';
 import { RedisModule } from 'src/redis/redis.module';
 import { TokenModule } from 'src/common/tokens/token.module';
+import { MessageModule } from 'src/messages/message.module';
+import { ChatModule } from 'src/chat/chat.module';
 
 @Module({
-  imports: [RedisModule, TokenModule],
+  imports: [RedisModule, TokenModule, ChatModule, MessageModule],
   providers: [ChatGateway, ChatSessionService],
   exports: [ChatGateway, ChatSessionService],
 })
